33d0835205e9a424af206c9c9914b2cc220cb08f
Kento Okamoto <kentokamoto@proton.me>
2026-01-29 08:38:37 -0800

    add vercel analytics and speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
 	import '../app.css';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	// Vercel Metrics and Monitoring
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 
 	let { children } = $props();
 </script>
