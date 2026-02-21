f6a3e2fca934084a394e38eb36193d69cf20823e
Kento Okamoto <kentokamoto@proton.me>
2026-02-21 00:08:07 -0800

    add menu for mobile screens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
 	import { AppBar } from '@skeletonlabs/skeleton-svelte';
 	import { MenuIcon } from '@lucide/svelte';
 	import mascotThumb from '$lib/assets/Slice-Enterprise-Mascot_thumb.webp?inline';
+
+	let isMenuOpen = $state(false);
+	function toggleMenu() {
+		isMenuOpen = !isMenuOpen;
+	}
 </script>
 
 <AppBar>
@@ -17,14 +22,31 @@
 			</a>
 		</AppBar.Lead>
 		<AppBar.Trail class="items-center justify-end">
-			<div class="hidden gap-4 md:block">
+			<div class="hidden gap-4 md:flex">
 				<a href="/slice-a-thon">Slice-a-thon</a>
 				<a href="/taco-ocho">Taco Ocho</a>
 				<a href="/eggnog">Eggnog Leg Jog</a>
 			</div>
-			<button type="button" class="btn-icon btn-icon-lg hover:preset-tonal md:hidden">
+			<button
+				type="button"
+				class="btn-icon btn-icon-lg hover:preset-tonal md:hidden"
+				onclick={toggleMenu}
+			>
 				<MenuIcon />
 			</button>
 		</AppBar.Trail>
 	</AppBar.Toolbar>
+	<nav class="block md:hidden" class:hidden={!isMenuOpen}>
+		<ul class="text-right">
+			<li>
+				<a href="/slice-a-thon">Slice-a-thon</a>
+			</li>
+			<li>
+				<a href="/taco-ocho">Taco Ocho</a>
+			</li>
+			<li>
+				<a href="/eggnog">Eggnog Leg Jog</a>
+			</li>
+		</ul>
+	</nav>
 </AppBar>
