2cd1f4ce68a1fb968d52b41e735ac154a75eed81
Kento Okamoto <kentokamoto@proton.me>
2026-02-20 23:36:40 -0800

    Responsive design for top layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
+	import { MenuIcon } from '@lucide/svelte';
+	import mascotThumb from '$lib/assets/Slice-Enterprise-Mascot_thumb.webp?inline';
+</script>
+
+<AppBar>
+	<AppBar.Toolbar class="grid-cols-[1fr_2fr] ">
+		<AppBar.Lead>
+			<a href="/" class="flex items-center">
+				<img
+					class="m-4 w-8 flex-none place-content-center object-scale-down"
+					src={mascotThumb}
+					alt="slice enterprise mascot"
+				/>
+				Slice Enterprise
+			</a>
+		</AppBar.Lead>
+		<AppBar.Trail class="items-center justify-end">
+			<div class="hidden gap-4 md:block">
+				<a href="/slice-a-thon">Slice-a-thon</a>
+				<a href="/taco-ocho">Taco Ocho</a>
+				<a href="/eggnog">Eggnog Leg Jog</a>
+			</div>
+			<button type="button" class="btn-icon btn-icon-lg hover:preset-tonal md:hidden">
+				<MenuIcon />
+			</button>
+		</AppBar.Trail>
+	</AppBar.Toolbar>
+</AppBar>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Welcome to Slice Enterprises</h1>
