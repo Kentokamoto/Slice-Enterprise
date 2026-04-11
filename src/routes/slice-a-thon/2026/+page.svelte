86ce959fdc2da141be4180d4ca273ab94e10fd70
Kento Okamoto <kentokamoto@proton.me>
2026-04-11 15:47:18 -0700

    add time

--- src/routes/slice-a-thon/2026/+page.svelte
+++ src/routes/slice-a-thon/2026/+page.svelte
@@ -9,7 +9,8 @@
 		MinusIcon,
 		PizzaIcon,
 		PlusIcon,
-		RulerDimensionLineIcon
+		RulerDimensionLineIcon,
+		ClockIcon
 	} from '@lucide/svelte';
 	import { Tabs } from '@skeletonlabs/skeleton-svelte';
 	const eventDate = new Date('May 16, 2026 4:00 PM PDT');
@@ -26,13 +27,20 @@
 
 <div class="bg-red-sauce/90 shadow-lg backdrop-blur-sm"><Counter judgementDate={eventDate} /></div>
 
-<div class="grid grid-cols-1 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+<div class="grid grid-cols-1 md:grid-cols-2 md:gap-1 lg:grid-cols-4">
 	<div class="glass-strong mx-5 my-1 rounded-2xl border border-black/10 p-3 shadow-md md:m-5">
 		<div class="flex items-center gap-2">
 			<CalendarFoldIcon />
 			<span class="font-bold">Date</span>
 		</div>
 		<div class="mt-2">May 16, 2026</div>
+	</div>
+	<div class="glass-strong mx-5 my-1 rounded-2xl border border-black/10 p-3 shadow-md md:m-5">
+		<div class="flex items-center gap-2">
+			<ClockIcon />
+			<span class="font-bold">Time</span>
+		</div>
+		<div class="mt-2">4:00 PM PDT</div>
 	</div>
 	<div class="glass-strong mx-5 my-1 rounded-2xl border border-black/10 p-3 shadow-md md:m-5">
 		<div class="flex items-center gap-2">
@@ -48,15 +56,18 @@
 		</div>
 		<div class="mt-2">Half Marathon (13.1 miles) and 10km</div>
 	</div>
-	<div class="glass-strong mx-5 my-1 rounded-2xl border border-black/10 p-3 shadow-md md:m-5">
-		<div class="flex items-center gap-2">
-			<PizzaIcon />
-			<span class="font-bold">Number of Pizza Stops</span>
-		</div>
-		<div class="mt-2 flex justify-center">3</div>
-	</div>
 </div>
 
+<div
+	class="glass-strong mx-5 my-1 max-w-full rounded-2xl border border-black/10 p-3
+		shadow-md md:m-5"
+>
+	<div class="flex items-center gap-2 whitespace-nowrap">
+		<PizzaIcon />
+		<span class="font-bold">Number of Pizza Stops</span>
+	</div>
+	<div class="mt-2">3</div>
+</div>
 <div class="glass-strong m-3 min-h-[30vh] rounded-2xl border border-black/10 p-3 shadow-md">
 	<Tabs
 		value={tabValue}
